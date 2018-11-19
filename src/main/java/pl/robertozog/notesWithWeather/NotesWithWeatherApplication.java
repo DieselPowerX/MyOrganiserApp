9c6fbc68a969702f9218a0c952dfb4195279422c
Robert Ozog <info@robertozog.com>
2018-11-19 09:57:24 +0100

    "first commit"

---
+++ src/main/java/pl/robertozog/notesWithWeather/NotesWithWeatherApplication.java
@@ -0,0 +1,12 @@
+package pl.robertozog.notesWithWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWithWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWithWeatherApplication.class, args);
+	}
+}
