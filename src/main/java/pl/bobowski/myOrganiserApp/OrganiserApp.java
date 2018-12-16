a4da62551df54ba67a350a71d13244b88f6120e4
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-12-16 13:20:01 +0100

    "settings"

--- src/main/java/pl/bobowski/myOrganiserApp/OrganiserApp.java
+++ src/main/java/pl/bobowski/myOrganiserApp/OrganiserApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesWithWeatherApplication {
+public class OrganiserApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotesWithWeatherApplication.class, args);
+		SpringApplication.run(OrganiserApp.class, args);
 	}
 }
