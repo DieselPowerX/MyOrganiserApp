bfd4891d73316446e472831a3b8f8117ef585056
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-12-16 13:16:30 +0100

    "settings"

---
+++ src/main/java/pl/bobowski/myOrganiserApp/model/UserSession.java
@@ -0,0 +1,17 @@
+package pl.bobowski.myOrganiserApp.model;
+
+import lombok.Data;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Data
+public class UserSession {
+
+    private boolean isLogin;
+    private int id;
+    private String loginUser;
+    private String city;
+}
